ef486bdd45982282f1cfee14fc7ba5c0664a1bbe
fds <frederic@codingame.com>
2017-05-31 19:26:57 +0200

    update

--- java-project/src/main/java/com/yourself/Example.java
+++ java-project/src/main/java/com/yourself/Example.java
@@ -13,7 +13,7 @@ public class Example {
 public int sum(int... ns) {
 	int sum = 0;
 	for(int i : ns){ 
-		sum = i; 
+		sum = i; // fix me!
 	} 
 	return sum;
 }
--- java-project/src/test/java/com/yourself/ExampleTest.java
+++ java-project/src/test/java/com/yourself/ExampleTest.java
@@ -7,6 +7,11 @@ public class ExampleTest {
 
 	@Test
 	public void test() {
-		Assert.assertEquals("Oops! sum(1, 5, -2) should return 4", 4, new Example().sum(1, 5, -2));
+		try {
+			Assert.assertEquals("Oops! sum(1, 5, -2) should return 4", 4, new Example().sum(1, 5, -2));
+		} catch (AssertionError ae) {
+			System.out.println("TECHIO> message -c Failure " + ae.getMessage());
+			throw ae;
+		}
 	}
 }

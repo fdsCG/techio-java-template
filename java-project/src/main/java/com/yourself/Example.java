4fb4533235e9a6011c4b609dffc12394dd6c01b9
fds <frederic@codingame.com>
2017-05-31 19:01:01 +0200

    update

--- java-project/src/main/java/com/yourself/Example.java
+++ java-project/src/main/java/com/yourself/Example.java
@@ -1,10 +1,20 @@
+// { autofold
 package com.yourself;
 
 import java.util.Arrays;
 
 public class Example {
-
-	public int sum(int... ns) {
-		return Arrays.stream(??).sum();
-	}
+// }
+/**
+ * Returns the sum of elements in an array of integers.
+ * 
+ * @param ns
+ *            an array of integers
+ * @return the sum of elements
+ */
+public int sum(int... ns) {
+	return Arrays.stream(??).sum();
 }
+//{ autofold
+}
+//}
--- java-project/src/test/java/com/yourself/ExampleTest.java
+++ java-project/src/test/java/com/yourself/ExampleTest.java
@@ -7,6 +7,6 @@ public class ExampleTest {
 
 	@Test
 	public void test() {
-		Assert.assertEquals("Did you return Hello World?", 4, Example.sum(1, 5, -2));
+		Assert.assertEquals("Oops! sum(1, 5, -2) should return 4", 4, new Example().sum(1, 5, -2));
 	}
 }

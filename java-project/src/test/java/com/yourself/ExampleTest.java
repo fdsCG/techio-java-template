f9963bc4851bcf7a8e010209f24ee759a8c7a06e
fds <frederic@codingame.com>
2017-06-02 16:50:03 +0000

    update

--- java-project/src/test/java/com/yourself/ExampleTest.java
+++ java-project/src/test/java/com/yourself/ExampleTest.java
@@ -10,13 +10,13 @@ public class ExampleTest {
 		try {
 			Assert.assertEquals("Running sum(2, 3)...", 5, new Example().sum(2, 3));
 			System.out.println("TECHIO> success true");
-			System.out.println("TECHIO> message --channel \"Congrats!\" \"If you're using Java 8, the Arrays class provides a stream(int[] array) method which returns a sequential IntStream with the specified int array.\"");
-			System.out.println("TECHIO> message --channel \"Congrats!\" \"int [] ns = {1,2,3,4};\"");
-			System.out.println("TECHIO> message --channel \"Congrats!\" \"int sum = Arrays.stream(arr).sum(); //prints 10\"");
+			System.out.println("TECHIO> message --channel \"Congrats! :)\" \"Did you know since java-8 is out you can use streams?\"");
+			System.out.println("TECHIO> message --channel \"Congrats! :)\" \"int [] ns = {1,2,3,4};\"");
+			System.out.println("TECHIO> message --channel \"Congrats! :)\" \"int sum = Arrays.stream(arr).sum(); //prints 10\"");
 		} catch (AssertionError ae) {
 			System.out.println("TECHIO> success false");
 			System.out.println("TECHIO> message --channel \"Oops :(\" \"" + ae.getMessage() + "\"");
-			System.out.println("TECHIO> message --channel \"Oops :(\" \"Did you try to accumulate all values into the sum variable?\"");
+			System.out.println("TECHIO> message --channel \"Oops :(\" \"Did you try to accumulate all values into the variable 'sum'?\"");
 		}
 	}
 }

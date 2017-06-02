ce8410f20df3c04c21d396f953adae9fefceef72
fds <frederic@codingame.com>
2017-06-02 15:56:58 +0000

    update

--- java-project/src/test/java/com/yourself/ExampleTest.java
+++ java-project/src/test/java/com/yourself/ExampleTest.java
@@ -8,7 +8,7 @@ public class ExampleTest {
 	@Test
 	public void test() {
 		try {
-			Assert.assertEquals("Running sum(1, 2, -2)...\\n", 1, new Example().sum(1, 2, -2));
+			Assert.assertEquals("Running sum(1, 2, -2)...", 1, new Example().sum(1, 2, -2));
 		} catch (AssertionError ae) {
 			System.out.println("TECHIO> message --channel \"Oops :(\" \"" + ae.getMessage() + "\"");
 

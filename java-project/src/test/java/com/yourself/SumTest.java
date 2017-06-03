bcc8f25049e1dcef6842a3368a82123649870dae
fds <frederic@codingame.com>
2017-06-03 10:23:57 -0700

    update

--- java-project/src/test/java/com/yourself/SumTest.java
+++ java-project/src/test/java/com/yourself/SumTest.java
@@ -12,7 +12,7 @@ public class SumTest {
 			System.out.println("TECHIO> success true");
 			System.out.println("TECHIO> message --channel \"Congrats! :)\" \"Did you know since java-8 is out you can use streams?\"");
 			System.out.println("TECHIO> message --channel \"Congrats! :)\" \"int [] ns = {1,2,3,4};\"");
-			System.out.println("TECHIO> message --channel \"Congrats! :)\" \"int sum = Arrays.stream(arr).sum(); //prints 10\"");
+			System.out.println("TECHIO> message --channel \"Congrats! :)\" \"int sum = Arrays.stream(ns).sum(); //prints 10\"");
 		} catch (AssertionError ae) {
 			System.out.println("TECHIO> success false");
 			System.out.println("TECHIO> message --channel \"Oops :(\" \"" + ae.getMessage() + "\"");

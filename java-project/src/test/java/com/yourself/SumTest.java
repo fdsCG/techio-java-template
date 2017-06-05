65b25b08493d3ff7cb0af1674251c97eb3b81f48
fds <frederic@codingame.com>
2017-06-04 20:33:49 -0700

    update

--- java-project/src/test/java/com/yourself/SumTest.java
+++ java-project/src/test/java/com/yourself/SumTest.java
@@ -12,6 +12,7 @@ public class SumTest {
 			Assert.assertEquals("Running sum(9, -3)...", 6, new Sum().sum(9, -3));
 			System.out.println("TECHIO> success true");
 			System.out.println("TECHIO> message --channel \"Kudos!\" \"Did you know since Java8 is out you can use streams?\"");
+			System.out.println("TECHIO> message --channel \"Kudos!\" ");
 			System.out.println("TECHIO> message --channel \"Kudos!\" \"int[] ns = {1, 2, 3};\"");
 			System.out.println("TECHIO> message --channel \"Kudos!\" \"int sum = Arrays.stream(ns).sum(); //prints 6\"");
 		} catch (AssertionError ae) {

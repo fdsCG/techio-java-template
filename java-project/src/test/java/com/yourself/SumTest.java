a55b0fac864176fabfe9d19fe654268e1a5ad7a7
fds <frederic@codingame.com>
2017-06-04 22:15:02 -0700

    update

--- java-project/src/test/java/com/yourself/SumTest.java
+++ java-project/src/test/java/com/yourself/SumTest.java
@@ -1,24 +1,67 @@
 package com.yourself;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SumTest {
+	
+	private static final String YODA =
+			"  __.-._\n" + 
+			"  '-._\"7'\n" + 
+			"   /'.-c\n" + 
+			"   |  /T\n" + 
+			"  _)_/LI";
 
 	@Test
-	public void test() {
+	public void test() throws IOException {
 		try {
 			Assert.assertEquals("Running sum(2, 3)...", 5, new Sum().sum(2, 3));
 			Assert.assertEquals("Running sum(9, -3)...", 6, new Sum().sum(9, -3));
-			System.out.println("TECHIO> success true");
-			System.out.println("TECHIO> message --channel \"Kudos!\" \"Did you know since Java8 is out you can use streams?\"");
-			System.out.println("TECHIO> message --channel \"Kudos!\" ");
-			System.out.println("TECHIO> message --channel \"Kudos!\" \"int[] ns = {1, 2, 3};\"");
-			System.out.println("TECHIO> message --channel \"Kudos!\" \"int sum = Arrays.stream(ns).sum(); //prints 6\"");
+			printSuccess(true);
+			
+			if (!existsInFile("Arrays.stream(ns).sum()", new File("./src/main/java/com/yourself/Sum.java"))) {
+				printMsg("Kudos!", "Did you know since Java8 is out you can use streams?\n");
+				printMsg("Kudos!", "int[] ns = {1, 2, 3};");
+				printMsg("Kudos!", "int sum = Arrays.stream(ns).sum(); //prints 6");
+			} else {
+				printMsg("You’re my personal Yoda.", YODA);
+			}
 		} catch (AssertionError ae) {
-			System.out.println("TECHIO> success false");
-			System.out.println("TECHIO> message --channel \"Oops :(\" \"" + ae.getMessage() + "\"");
-			System.out.println("TECHIO> message --channel \"Hint\" \"Did you try to accumulate all values into the variable 'result'?\"");
+			printSuccess(false);
+			printMsg("Oops :(", ae.getMessage());
+			printMsg("Hint", "Did you try to accumulate all values into the variable 'result'?");
+		}
+	}
+
+	private static void printMsg(String channel, String msg) {
+		msg = msg.replaceAll("\"", "\\\\\"");
+		for (String s : msg.split("\\n")) {
+			System.out.println(String.format("TECHIO> message --channel \"%s\" \"%s\"", channel, s));
+		}
+	}
+
+	private static void printSuccess(boolean success) {
+		System.out.println(String.format("TECHIO> success %s", success));
+	}
+
+	// check if a particular text belongs to a file
+	private static boolean existsInFile(String text, File srcFile) throws FileNotFoundException {
+		Scanner scanner = new Scanner(srcFile);
+		try {
+			while (scanner.hasNextLine()) {
+				String lineFromFile = scanner.nextLine();
+				if (lineFromFile.contains(text))
+					return true;
+
+			}
+			return false;
+		} finally {
+			scanner.close();
 		}
 	}
 }

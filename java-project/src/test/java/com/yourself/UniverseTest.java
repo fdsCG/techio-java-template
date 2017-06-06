188ed9b6d01d6fbc698f94113a25bc40ef782c46
fds <frederic@codingame.com>
2017-06-05 18:13:28 -0700

    update

--- java-project/src/test/java/com/yourself/UniverseTest.java
+++ java-project/src/test/java/com/yourself/UniverseTest.java
@@ -28,7 +28,7 @@ public class UniverseTest {
 			} else {
 				msg("Kudos 🌟", "Did you know since Java8 is out you can use streams? Try it!");
 				msg("Kudos 🌟", "");
-				msg("Kudos 🌟", "int[] ns = {37, 2, 3};");
+				msg("Kudos 🌟", "int[] galaxies = {37, 2, 3};");
 				msg("Kudos 🌟", "int sum = Arrays.stream(galaxies).sum(); //prints 42");
 			}
 		} catch (AssertionError ae) {
